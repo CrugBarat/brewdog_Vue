a4893f519e02805c7a21960709224c4df259c7d1
Craig Barratt <craigtbarratt@gmail.com>
2020-04-23 13:29:38 +0100

    adds list and list item

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,38 @@
 <template lang="html">
-
+  <div>
+    <h1>Beers</h1>
+    <beer-list :beers="beers" v-model="selectedBeer"></beer-list>
+  </div>
 </template>
 
 
 <script>
+
+import BeerList from './components/BeerList.vue';
+
+import {eventBus} from './main.js'
+
 export default {
   name: 'app',
   data () {
     return {
       beers: [],
+      selectedBeer: null
     }
   },
-    mounted() {
-      fetch('https://api.punkapi.com/v2/beers')
-      .then(res => res.json())
-      .then(beers => this.beers = beers)
-    }
+  mounted() {
+    fetch('https://api.punkapi.com/v2/beers')
+    .then(res => res.json())
+    .then(beers => this.beers = beers)
+
+    eventBus.$on('selected-beer', (beer) => {
+      this.selectedBeer = beer;
+    });
+
+  },
+  components: {
+    "beer-list": BeerList
+  }
 }
 </script>
 
---
+++ src/components/BeerList.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <div>
+  <list-item v-for="(beer, index) in beers" :beer="beer" :key="index"></list-item>
+  </div>
+</template>
+
+<script>
+
+import ListItem from './ListItem.vue'
+
+import {eventBus} from '../main.js'
+
+export default {
+  name: 'beer-list',
+  props:['beers'],
+  components: {
+    'list-item': ListItem
+  }
+}
+
+</script>
+
+<style lang="css" scoped>
+</style>
